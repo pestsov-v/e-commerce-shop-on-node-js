1904996929722225d763529a7dbfc024f8d95fe5
Vlad <pestsov.js@gmail.com>
2021-04-12 22:28:34 +0300

    create validation.
    all innovation in app:
    - created a basic validation in registration page and courses page.
    - created a 404 page.
    - created a profile page.
    - realized function load the imges in profile user.

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
+const profileRoutes = require('./routes/profile')
+const errorHanler = require('./midlleware/error')
 const varMiddleware = require('./midlleware/variables')
+const fileMiddleware = require('./midlleware/file')
 const UserMiddleware = require('./midlleware/user')
+
 const keys = require('./keys')
 
 const app = express()
@@ -33,6 +37,7 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: keys.SESSION_SECRET,
@@ -40,6 +45,7 @@ app.use(session({
   saveUninitialized: false,
   store
 }))
+app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
 app.use(varMiddleware)
@@ -52,6 +58,9 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
+
+app.use(errorHanler)
 
 const PORT = process.env.PORT || 3000
 
--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
 const Course = require('../models/course')
 const auth = require('../midlleware/auth')
+const {validationResult} = require ('express-validator')
+const {courseValidators} = require('../utils/validators')
 const router = Router()
 
 router.get('/', async (req, res) => {
@@ -24,7 +26,6 @@ router.get('/:id/edit', auth, async (req, res) => {
   if (!req.query.allow) {
     return res.redirect('/')
   }
-  
   try {
     const course = await Course.findById(req.params.id)
 
@@ -41,9 +42,16 @@ router.get('/:id/edit', auth, async (req, res) => {
   }
 })
 
-router.post('/edit', auth, async (req, res) => {
+router.post('/edit', auth, courseValidators, async (req, res) => {
+  const errors = validationResult(req)
+  const {id} = req.body
+
+  if (!errors.isEmpty()) {
+    return res.status(422).redirect(`/courses/${id}/edit?allow=true`)
+  }
+
   try {
-    const {id} = req.body
+
     delete req.body.id
     const course = await Course.findById(id)
     if (course.userId.toString() !== req.user._id.toString()) {
