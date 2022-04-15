50e33af2cf6957fd9b0889e91cda56922401ee04
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-15 13:54:26 +0300

    create the session and basket
    connect user to mongoStore when user authorization in site.
    
    create the basket form and puth to basket method

--- midlleware/user.js
+++ midlleware/user.js
@@ -1,10 +1,10 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
-module.exports = async function(req, res, next) {
-    if (!req.session.user) {
-        return next()
-    }
-    
-    req.user = await User.findById(req.session.user._id)
-    next()
-}
+module.exports = async function (req, res, next) {
+  if (!req.session.user) {
+    return next();
+  }
+  console.log(req.user);
+  req.user = await User.findById(req.session.user._id);
+  next();
+};
